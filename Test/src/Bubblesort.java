25e51c77f83afc748ff795c103e9088de6871fb3
DOLLY <DOLLY@LAPTOP-JFUETIB9>
2020-05-16 10:42:30 +0530

    commit

---
+++ Test/src/Bubblesort.java
@@ -0,0 +1,26 @@
+
+public class Bubblesort {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a= {10,29,67,90,78};
+		int temp ;
+		for(int i=0;i<a.length;i++) {
+			for(int k=0;k<a.length-1;k++) {
+				if(a[k]>a[k+1])
+				{
+					temp=a[k];
+					a[k]=a[k+1];
+					a[k+1]=temp;
+				}
+			}
+			
+		}
+		for (int m = 0; m< a.length; m++) {
+			System.out.print("" +  a[m]+"");
+			
+		}
+	}
+
+}
